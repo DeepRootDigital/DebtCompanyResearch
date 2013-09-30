edf3fec8fab2e8f3c7c9017ca7d21bf3584bcbdf
Nicholas Koskowski <Nick@businessonmarketst.com>
2013-09-30 16:34:20 -0700

    V.99 Update Revised
    Added v.99 update items
    Must Credit Sara
    Added some footer code

--- function.js
+++ function.js
@@ -1,6 +1,9 @@
-//Data Serialization
 $(document).ready(function(){
 
+	//Default Animations
+	$('.clouds').pan({fps: 20, speed: .60, dir: 'left'});
+
+	//Data Serialization
 	$('.debt-form-app .step-one button').click(function(event){
 		event.preventDefault();
 		$('.step-one').fadeOut();
@@ -16,6 +19,43 @@ $(document).ready(function(){
 	$('.debt-form-app .step-three button').click(function(event){
 		event.preventDefault();
 		$('.step-three').fadeOut();
+	});
+
+	//Tab Functionality
+
+	function TabBuffer() {
+		$('.tab-one').hide();
+		$('.tab-two').hide();
+		$('.tab-three').hide();
+		$('.tab-four').hide();
+		$('.tab').removeClass('tab-hover');
+	};
+
+	$('.tab').click(function(){
+		if ( $(this).hasClass('one') ) {
+			TabBuffer();
+			$(this).addClass('tab-hover');
+			$('.tab-one').show();
+		}
+
+		if ( $(this).hasClass('two') ) {
+			TabBuffer();
+			$(this).addClass('tab-hover');
+			$('.tab-two').show();
+		}
+
+		if ( $(this).hasClass('three') ) {
+			TabBuffer();
+			$(this).addClass('tab-hover');
+			$('.tab-three').show();
+		}
+
+		if ( $(this).hasClass('four') ) {
+			TabBuffer();
+			$(this).addClass('tab-hover');
+			$('.tab-four').show();
+		}
+		console.log('event fired!');
 	});
 
 });
